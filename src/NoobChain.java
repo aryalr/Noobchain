137180893826f794d63e8a21cc392fffdc7cbb2a
aryalr <aryajati@gmail.com>
2025-02-24 18:53:11 +0700

    add the gson file library to lib folder and make function to chech the hash is valid

--- src/NoobChain.java
+++ src/NoobChain.java
@@ -3,6 +3,40 @@ import com.google.gson.GsonBuilder;
 
 public class NoobChain {
 
+    public static Boolean isChainValid() {
+
+        Block currentBlock;
+        Block previousBlock;
+
+        // looping through blockchain to check the hashes
+        for (int i = 1; i < blockchain.size(); i++) {
+            currentBlock = blockchain.get(i);
+            previousBlock = blockchain.get(i-1);
+            
+            //comparing the registered hash and calculated hash
+            if(!currentBlock.hash.equals(currentBlock.calculateHash()) ) {
+                System.out.println("Hash is not equal!");
+                return false;
+            }
+
+            //compare previous hash and registered previous hash
+            if (!previousBlock.hash.equals(currentBlock.previousHash)) {
+                System.out.println("Previous hash is not equal!");
+                return false;
+            }
+        }
+        return true;
+
+    }
+
+
+
+
+
+
+
+
+
     public static ArrayList<Block> blockchain = new ArrayList<Block>();
 
     public static void main(String[] args) {
